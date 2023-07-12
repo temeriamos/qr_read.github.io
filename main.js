309eac0545313030579d37b9b2d32f8771b06c85
temeriamos <czipo14@gmail.com>
2023-07-12 18:06:31 +0200

    Modificar js con libreria oficial.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// si el scan es correcto se muestra el resultado en el div resultado
+/*// si el scan es correcto se muestra el resultado en el div resultado
 function onScanSuccess(qrCodeMessage) {
     document.getElementById("resultado").innerHTML =
       '<span class="result">' + qrCodeMessage + "</span>";
@@ -17,4 +17,23 @@ function onScanSuccess(qrCodeMessage) {
   
   // in
   html5QrCodeScanner.render(onScanSuccess, onScanError);
+  */
   
+  //.----
+
+  function onScanSuccess(decodedText, decodedResult) {
+    // handle the scanned code as you like, for example:
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+  }
+  
+  function onScanFailure(error) {
+    // handle scan failure, usually better to ignore and keep scanning.
+    // for example:
+    console.warn(`Code scan error = ${error}`);
+  }
+  
+  let html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader",
+    { fps: 10, qrbox: {width: 250, height: 250} },
+    /* verbose= */ false);
+  html5QrcodeScanner.render(onScanSuccess, onScanFailure);
