f45f404ce93d67530837cda3e0139f3aa179fd14
temeriamos <czipo14@gmail.com>
2023-07-12 20:09:12 +0200

    Mejora para usar la camara solo con la camara trasera, sin elegir opciones de lentes.

--- main.js
+++ main.js
@@ -1,25 +1,4 @@
-/*// si el scan es correcto se muestra el resultado en el div resultado
-function onScanSuccess(qrCodeMessage) {
-    document.getElementById("resultado").innerHTML =
-      '<span class="result">' + qrCodeMessage + "</span>";
-  }
-  
-  // Si el scan es incorrecto podemos mostrar un error
-  function onScanError(errorMessage) {
-    // Handle Scan Error
-  }
-  
-  // Setting Qr Scanner properties
-  var html5QrCodeScanner = new Html5QrcodeScanner("lector", {
-    fps: 10,
-    qrbox: 250
-  });
-  
-  // in
-  html5QrCodeScanner.render(onScanSuccess, onScanError);
-  */
-  
-  //.----
+/*
 
   function onScanSuccess(decodedText, decodedResult) {
     // handle the scanned code as you like, for example:
@@ -37,5 +16,27 @@ function onScanSuccess(qrCodeMessage) {
   let html5QrcodeScanner = new Html5QrcodeScanner(
     "reader",
     { fps: 10, qrbox: {width: 250, height: 250} },
-    /* verbose= */ false);
+    false);
   html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+  */
+
+  const html5QrCode = new html5QrCode("reader")
+  const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    document.getElementById("resultado").innerHTML =
+    '<span class="result">' + decodedText + decodedResult + "</span>";
+  };
+
+  const config = {
+    fps: 10,
+    qrbox: {
+      with: 250,
+      height: 250
+    }
+  };
+
+  html5QrCode.start(
+    {
+      facingMode: "environment"
+    },
+    config, qrCodeSuccessCallback
+    )
