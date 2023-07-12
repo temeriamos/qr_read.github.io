528dec71a63bfbec77bf20633d50c969cc414b98
temeriamos <czipo14@gmail.com>
2023-07-12 18:14:52 +0200

    Actualizar main.js y muestre resultado en #resultado

--- main.js
+++ main.js
@@ -24,6 +24,8 @@ function onScanSuccess(qrCodeMessage) {
   function onScanSuccess(decodedText, decodedResult) {
     // handle the scanned code as you like, for example:
     console.log(`Code matched = ${decodedText}`, decodedResult);
+    document.getElementById("resultado").innerHTML =
+      '<span class="result">' + decodedText + decodedResult + "</span>";
   }
   
   function onScanFailure(error) {
