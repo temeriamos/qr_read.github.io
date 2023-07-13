7c1116d23df2aac9d824a1486b5d8f9f38390f6e
temeriamos <czipo14@gmail.com>
2023-07-13 09:53:13 +0200

    mejora, en añadir boton iniciar - stop

--- main.js
+++ main.js
@@ -1,26 +1,6 @@
-/*
-
-  function onScanSuccess(decodedText, decodedResult) {
-    // handle the scanned code as you like, for example:
-    console.log(`Code matched = ${decodedText}`, decodedResult);
-    document.getElementById("resultado").innerHTML =
-      '<span class="result">' + decodedText + decodedResult + "</span>";
-  }
-  
-  function onScanFailure(error) {
-    // handle scan failure, usually better to ignore and keep scanning.
-    // for example:
-    console.warn(`Code scan error = ${error}`);
-  }
-  
-  let html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader",
-    { fps: 10, qrbox: {width: 250, height: 250} },
-    false);
-  html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-  */
 
   const html5QrCode = new Html5Qrcode("reader", true)
+  
   const qrCodeSuccessCallback = (decodedText, decodedResult) => {
     document.getElementById("resultado").innerHTML =
     '<span class="result">' + decodedText + decodedResult + "</span>";
@@ -35,9 +15,23 @@
     }
   };
 
-  html5QrCode.start(
-    {
-      facingMode: "environment"
-    },
-    config, qrCodeSuccessCallback
-    )
+  function startScan(){
+    console.log("Start Scan")
+    html5QrCode.start(
+      {
+        facingMode: "environment"
+      },
+      config, qrCodeSuccessCallback
+      )
+  }
+
+
+  function stopScan(){
+    html5QrCode.stop().then((ignore) => {
+      console.log("Stop Scan")
+    }).catch((err) => {
+
+    });
+  }
+
+ 
